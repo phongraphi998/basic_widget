253736ed6362b1c57daeb916bb5d6aaa31e17a42
phongraphi998 <pamnata1987@gmail.com>
2026-01-08 21:43:05 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Basic widget'),
+          backgroundColor: Colors.purple.shade700,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/center_image.gif', 
+                width: 250,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Hello world',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Text('+'),
         ),
       ),
     );
